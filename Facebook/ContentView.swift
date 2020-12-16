333a698f3575d69ce627e1d83ae4b6dd40489a88
Afraz Siddiqui <afsiddiq@microsoft.com>
2020-12-16 06:55:04 -0500

    Initial Commit

---
+++ Facebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facebook
+//
+//  Created by Afraz Siddiqui on 12/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Facebook/FacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookApp.swift
+//  Facebook
+//
+//  Created by Afraz Siddiqui on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct FacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
